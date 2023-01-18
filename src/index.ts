3e885904e415f1a9543b430de65d6314be7287ba
Michael Gartner <mclicks@gmail.com>
2023-01-17 19:40:15 -0800

    replace \n\n with \n

--- src/index.ts
+++ src/index.ts
@@ -92,7 +92,7 @@ export default runExtension({
         if (e.highlight != "")
           output += formatHighlightBasedOnTemplate(
             hTemplate,
-            e.highlight,
+            e.highlight.replace(/\n\n/g, '\n'),
             e.context
           );
         output = output.trim();
